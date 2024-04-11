5e41b81b4c9dc4dfad20677ffd22bc13c3e521a1
Henrique <aaluiziohenrique22@gmail.com>
2024-04-11 17:20:36 -0300

    Tipos_de_Dados

---
+++ Python_basico/tipos_de_dados.py
@@ -0,0 +1,10 @@
+# int - números inteiros
+print(1 + 12 + 100)
+# float - números de pontos flutuantes
+print(1.5 + 1 + 0.5)
+# bools - boleano 
+print(True)
+print(False)
+# str - string
+print('Python')
+
