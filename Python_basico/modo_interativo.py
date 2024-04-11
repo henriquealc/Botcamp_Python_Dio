8140d809515a5987db14178bb1ac422414916062
Henrique <aaluiziohenrique22@gmail.com>
2024-04-11 17:20:20 -0300

    Modo_interativo

---
+++ Python_basico/modo_interativo.py
@@ -0,0 +1,3 @@
+#'''https://web.dio.me/course/conhecendo-a-linguagem-de-programacao-python/learning/0c44b229-a31e-4b20-874c-57adaed64fb1?back=/track/coding-future-vivo-python-ai-backend-developer&tab=undefined&moduleId=undefined'''
+
+print(5//2)
