428e5be11878625d674dc399372932712c0e5090
Henrique <aaluiziohenrique22@gmail.com>
2024-04-16 11:33:18 -0300

    Funções entrada e saida

---
+++ Python_basico/funcoes_entrada_e_saida.py
@@ -0,0 +1,16 @@
+# nome = 'Henrique'
+# sobrenome = 'Ferreira'
+
+# print(nome, sobrenome)
+# print(nome, sobrenome, end='...\n') # \n quebra de linha
+# print(nome, sobrenome, sep= '#') # sep - separador
+
+
+'''Exemplo de como usar o input'''
+nome = input('Informe o seu nome: ')
+idade = int(input('Informe a sua idade: '))
+
+print(nome, idade)
+print(nome, idade, end='...\n')
+print(nome, idade, sep='#', end='\n')
+print(nome, idade, sep='#')     
