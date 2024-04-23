92489f558730394c827a50c4a87481bb2eb2d843
Henrique <aaluiziohenrique22@gmail.com>
2024-04-23 14:22:12 -0300

    Operadores de comparacao

---
+++ Python_basico/operadores_de_comparacao.py
@@ -0,0 +1,10 @@
+'''Retorna Valores bool = True ou False'''
+
+saldo = 450
+saque = 200
+print(saldo == saque) # Verifica se saldo é igual a saque
+print(saldo != saque) # '' ''  ''  '' é diferente a saque
+print(saldo > saque) # Saldo é maior que saque
+print(saldo < saque) # Saldo menor que saque
+print(saldo <= saque) # Saldo menor ou igual a saque
+print(saldo >= saque) # Saldo maior ou igual a saque
