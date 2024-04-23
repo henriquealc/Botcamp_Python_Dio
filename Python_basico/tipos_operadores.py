fe527ea7821b89d1c7477239c0e564903a9fd0b1
Henrique <aaluiziohenrique22@gmail.com>
2024-04-23 13:57:46 -0300

    Tipos de Operadores no Python

---
+++ Python_basico/tipos_operadores.py
@@ -0,0 +1,51 @@
+# # Adição
+# print('ADIÇÃO')
+# print(1 + 1)
+
+# # Subtração
+# print()
+# print('SUBTRAÇÃO')
+# print(10 - 2)
+
+# # Multiplicação
+# print()
+# print('MULTIPLICAÇÃO')
+# print(4 * 3)
+
+# # Divisão
+# print()
+# print('DIVISÃO')
+# print(12/ 3)
+
+# # Divisão inteira
+# print()
+# print('DIVISÃO INTEIRA')
+# print(12//2)
+
+# # Módulo
+# print()
+# print('MÓDULO')
+# print(10 % 3)
+
+# # Exponenciação
+# print()
+# print('EXPONENCIAÇÃO')
+# print(2 ** 3)
+
+
+'''MAIS EXEMPLOS'''
+# produto1 = 10
+# produto2 = 20
+
+# print(produto1 + produto2)
+# print(produto1 - produto2)
+# print(produto1 * produto2)
+# print(produto1 / produto2)
+# print(produto1 // produto2)
+# print(produto1 % produto2)
+# print(produto1 ** produto2)
+
+'''EXEMPLO DE EXPRESÃO'''
+
+x = (10 + 5) * 4
+print(x)
