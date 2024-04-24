8f7134f0ddd6c2dcd74c1b853d690fa7be94908b
Henrique <aaluiziohenrique22@gmail.com>
2024-04-24 11:54:51 -0300

    Operdores de Atribuicao

---
+++ Python_basico/operadores_de_atribuicao.py
@@ -0,0 +1,28 @@
+# Atribuição Simples
+
+saldo = 500
+print(saldo) #500
+
+# Atribuição com adição
+saldo = 500
+saldo += 200 # saldo igual + 200: total 700
+print(saldo) #700
+
+# Atribuição com divisão
+saldo = 500
+saldo /= 5
+print (saldo) #100.0
+
+saldo = 500
+saldo //= 5
+print(saldo) #100
+
+# Atribuição com módulo
+saldo = 500
+saldo %= 480
+print(saldo) #20
+
+# Atribuição com exponenciação
+saldo = 80 
+saldo **= 2
+print(saldo) #6400
