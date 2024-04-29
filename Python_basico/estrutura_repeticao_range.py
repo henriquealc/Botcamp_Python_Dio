c6d1bc9ec526c4b7cbc5b68d26ec2da508507c20
Henrique <aaluiziohenrique22@gmail.com>
2024-04-29 12:25:25 -0300

    Estrutura de repeticao com range

---
+++ Python_basico/estrutura_repeticao_range.py
@@ -0,0 +1,21 @@
+# range(stop) -> range object
+# range (start, stop[, step]) -> range object
+
+# lista = list(range(4))
+# print(lista)
+
+'EX de uso do range com for'
+# num = int(input('Informe o número: '))
+# for n in (range(num)):
+#     n += 1
+#     print(n)
+
+# for numero in range(0, 11):
+#     print(numero, end=' ')
+
+
+
+# Exibindo a tabuada do 5
+# Exemplo ultilizando a função built-in range
+for numero in range(0, 51, 5): # começa, fim, pula
+    print(numero, end=' ') # emd - usado para colocar um do lado do outro
